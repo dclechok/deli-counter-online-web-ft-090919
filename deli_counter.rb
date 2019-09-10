b9920efa38a5898c6359e3906463c6e1d18bc8c9
Daniel Lechok <dclechok@gmail.com>
2019-09-10 19:11:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,13 @@
-require 'pry'
-
 def line(deli)
   if deli.empty?
-    puts "The line is currently empty."
+    return "The line is currently empty."
   end
 
-  print "The line is currently: " 
+  puts "The line is currently: \n" 
   deli.each_with_index do |name, index|
 		index += 1
-    print index.to_s + ". " + name + " "
-   end
-  # binding.pry
+    puts index.to_s + ". " + name + " "
+  end
 end
 
 
