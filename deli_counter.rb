1be7cea315681bfca2db05f4711e2a7dafb362a6
Daniel Lechok <dclechok@gmail.com>
2019-09-10 18:53:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(deli)
     line.each_with_index do |name, index|
       # "The line is currently: 1. Logan 2. Avi 3. Spencer
       index.to_s + ". " + name + " "
-
+    end
 end
 
 def take_a_number(current_people, s_name)
