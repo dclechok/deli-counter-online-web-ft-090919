e129c838e1fe05385c0261ede712f3ef90897cb2
Daniel Lechok <dclechok@gmail.com>
2019-09-10 18:49:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,9 @@ def line(deli)
   if line.empty?
     return "The line is currently empty."
   else
-    line.
+    line.each_with_index do || 
+      "The line is currently: 1. Logan 2. Avi 3. Spencer"
+    end
 end
 
 def take_a_number(current_people, s_name)
